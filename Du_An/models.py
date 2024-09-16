a61c44865589bc7b44304bde82ec3b018d7f31e8
bao-123 <baomap040611vn@gmail.com>
2024-09-16 19:44:05 +0700

    Working on

--- Du_An/models.py
+++ Du_An/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Teacher(User):
+    subject = models.CharField(max_length=200)
+    is_boy = models.BooleanField()
+
+
+class Parent(User):
+    children = models.ManyToManyField("Student", related_name="parent")
+
+    
+class Student(models.Model):
+    full_name = models.CharField(max_length=200)
+    birthday = models.DateField(blank=False)
+    is_boy = models.BooleanField()
+    math = models.ForeignKey("MainSubject", on_delete=models.PROTECT, related_name="student")
+
+
+class Class(models.Model):
+    form_teacher = models.OneToOneField(Teacher, on_delete=models.CASCADE, related_name="form_class")
+    name = models.CharField(max_length=3)
+    students = models.ManyToManyField(Student, blank=True, related_name="Class")
+    math_teacher = models.ForeignKey(Teacher, blank=True, on_delete=models.DO_NOTHING, related_name="math_teaching_class")
+
+    def student_count(self) -> int:
+        return self.students.count()
+    
+    def remove_teacher(self, subject, teacher: Teacher) -> None:
+        match(subject):
+            case "math":
+                self.math_teacher = None
+            #TODO
+
+class MainSubject(models.Model):
+    diem_thuong_xuyen1 = models.FloatField(blank=True)
+    diem_thuong_xuyen2 = models.FloatField(blank=True)
+    diem_thuong_xuyen3 = models.FloatField(blank=True)
+    diem_thuong_xuyen4 = models.FloatField(blank=True)
+    diem_giua_ki = models.FloatField(blank=True)
+    diem_cuoi_ki = models.FloatField(blank=True)
+    comment = models.TextField()
+    
+
--- Du_An/views.py
+++ Du_An/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, HttpResponseNotAllowed
+from django.contrib.auth import login, authenticate, logout
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Ok")
+    pass
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "Du_An/login.html")
+    elif request.method == "POST":
+        username: str = request.POST["username"]
+        password = request.POST["password"]
+
+        if "role" not in list(request.POST.keys()):
+            #replace this
+            return HttpResponseBadRequest("please choose either teacher or parent")
+        
+        user_type = request.POST["role"]
+
+        try:
+            if user_type == "teacher":
+                user = Teacher.objects.get(username=username, password=password)
+            elif user_type == "parent":
+                user = Parent.objects.get(username=username, password=password)
+            user = authenticate(request, username=username, password=password)
+
+        except:
+            user = None
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            #replace this
+            return HttpResponseBadRequest("Invalid password or username")
+    else:
+        return HttpResponseNotAllowed("method not allowed.")
+            
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "Du_An/register.html")
+    elif request.method == "POST":
+        pass
+    else:
+        return HttpResponseNotAllowed("method not allowed.")
