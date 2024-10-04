a19ed6653bcdec8827e049d34ce8ff471b6ed45b
bao-123 <baomap040611vn@gmail.com>
2024-10-04 13:16:27 +0700

    Working on tests

--- Du_An/tests.py
+++ Du_An/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .utils import TestUtils
+from .models import *
 
 # Create your tests here.
+
+class ProjectTest(TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.client = Client()
+
+        #create some subjects
+        self.subjects = TestUtils.create_subjects(["Toán", "Ngữ Văn", "Tiếng Anh"])
+
+        #create some classes
+        self.classes = TestUtils.create_classes(["6a1", "6a2", "6a3"])
+
+        #create some teachers
+        self.teacher1 = TestUtils.create_user(type="teacher", email="teacher1@teacher.com",
+                                              password="teacher1", full_name="Nguyễn Văn A",
+                                              subjects=self.subjects[0])
+        self.teacher2 = TestUtils.create_user(type="teacher", email="teacher2@teacher.com", 
+                                              password="teacher2", full_name="Nguyễn Văn B", 
+                                              subjects=[self.subjects[0], self.subjects[1]])
+        
+        #TODO: create some students to test register page
+
+    def test_welcome_page(self):
+        response = self.client.get(reverse(""))
+
+        self.assertEqual(response.status_code, 200)
+        print("Test welcome page finished. ✔")
+
+    def test_login_page(self):
+        response = self.client.get(reverse("login"))
+
+        self.assertEqual(response.status_code, 200)
+        print("test login page finished. ✔")
+    
+    def test_register_page(self):
+        response = self.client.get(reverse("register"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["subjects"].count(), 3)
+        self.assertEqual(response.context["classes"].count(), 3)
+        print("Test register page finished. ✔")
--- Du_An/utils.py
+++ Du_An/utils.py
@@ -1,1 +1,59 @@
-pass
+from .models import *
+
+#** functions for testing purpose
+class TestUtils():
+
+    #! Only use these functions for testing purpose!
+    @staticmethod
+    def create_subjects(names: list[str]):
+        subjects = []
+        for name in names:
+            subjects.append(Subject.objects.create(name=name))
+        return subjects
+    
+    @staticmethod
+    def create_classes(names: list[str]) -> list[Class]:
+        classes_count = len(names)
+        class_list = []*classes_count
+        for i in range(classes_count):
+            classroom = Class.objects.create(name=names[i])
+            class_list[i] = classroom
+        return class_list
+    
+    #TODO
+    @staticmethod
+    def create_student(name: str, classroom: Class, role: str | None = None, is_boy: bool | None = None):
+        pass
+
+    @staticmethod
+    def create_user(type: str | None = None, 
+                    email: str | None = None, 
+                    password: str | None = None, 
+                    full_name: str | None = None,
+                    children: list[Student] | None = None,
+                    subjects: list[Subject] | None = None
+                    ) -> Parent | Teacher | None:
+        try:
+            if type == "teacher":
+                #** We not user 'make_password' just for testing purpose
+                teacher = Teacher(full_name=full_name, username=email, password=password)
+
+                if subjects:
+                    teacher.subject.set(subjects)
+                
+                teacher.save()
+                return teacher
+            elif type == "parent":
+                parent = Parent(full_name=full_name, username=email, password=password)
+
+                if children:
+                    parent.children.set(children)
+
+                parent.save()
+
+                return parent
+            else:
+                raise Exception("Unknow user type")
+        except Exception as e:
+            print(e)
+            return None
