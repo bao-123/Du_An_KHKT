8ef1b9e57a3e6efcc7430a1916b8287b14275507
bao-123 <baomap040611vn@gmail.com>
2024-10-26 19:04:13 +0700

    Working on animations

--- Du_An/static/Du_An/js/list.js
+++ Du_An/static/Du_An/js/list.js
@@ -6,18 +6,27 @@ document.addEventListener("DOMContentLoaded", () => {
     classes.forEach(element => {
         const showStudentBtn = element.querySelector(".showStudentBtn");
         const btnIcon = showStudentBtn.querySelector("i");
+        const studentsDiv = element.querySelector(".student_list");
 
+        //* Toggle the display of the students list when the button is clicked.
         showStudentBtn.addEventListener("click", () =>{
-            const studentsDiv = element.querySelector(".student_list");
 
+            //* Toggle the icon and the display of the students list.
             if(studentsDiv.style.display === "none")
             { //TODO: add animation
                 studentsDiv.style.display = "block";
+                studentsDiv.style.animation = "height-appear 0.5s ease-in-out";
                 btnIcon.className = "fa-solid fa-angle-down";
             }
             else
             {
-                studentsDiv.style.display = "none";
+                //studentsDiv.childNodes.forEach(node => {node.style? node.style.display = "none" : '';});
+                studentsDiv.style.animation = "height-disappear 0.5s ease-in-out";
+                studentsDiv.addEventListener("animationend", function hideDiv() {
+                    studentsDiv.style.display = "none";
+                    studentsDiv.removeEventListener("animationend", hideDiv);
+                });
+
                 btnIcon.className = "fa-solid fa-angle-up";
             }
         });
