4145e32ac482e870234593ac57f3e108a62e2b02
bao-123 <baomap040611vn@gmail.com>
2024-10-31 13:03:27 +0700

    Finish change user's full name form

--- Du_An/static/Du_An/js/index.js
+++ Du_An/static/Du_An/js/index.js
@@ -1,6 +1,12 @@
+import { changeUserInfo, displayMessage } from "./utils.js";
+
+const displayMessageDivId = "displayMessage"; //-i the id of the div that will display messages.
+
 document.addEventListener("DOMContentLoaded", () => {
     //*show change infomation when hover
     const userInfoDivs = document.querySelectorAll(".userInfo");
+    const changeUsernameForm = document.getElementById("changeFullNameForm");
+    const userFullName = document.getElementById("userFullName");
 
     userInfoDivs.forEach(element => {
         element.addEventListener("mouseenter", () => {
@@ -10,7 +16,33 @@ document.addEventListener("DOMContentLoaded", () => {
         element.addEventListener("mouseleave", () => {
             element.querySelector(".changeInfoForm").classList.remove("show");
         }); 
-
     });
 
+    changeUsernameForm.addEventListener("submit", async event => {
+        event.preventDefault();
+
+        const newFullName = changeUsernameForm.querySelector("#fullNameInput").value;
+        const response = await changeUserInfo("full_name", newFullName);
+        if(!response)
+        {
+            displayMessage(displayMessageDivId,
+                "Lỗi",
+                "Lỗi xảy ra khi thay đổi họ tên, vui lòng thử lại sau",
+                "error",
+                "md");
+            return;
+        }
+
+        displayMessage(displayMessageDivId,
+            response.status === 200 ? "Thành công" : "Lỗi",
+            response.message,
+            response.status === 200? "success" : "error",
+            "md"
+        );
+
+        if(response.status === 200)
+        {
+            document.getElementById("userFullName").textContent = `Chào ${newFullName}`;
+        }
+    });
 });
